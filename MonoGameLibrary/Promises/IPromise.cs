71a6561e949f159767fb507d75d0597117e13521
Paul Vladimirov <dr.scroodge@gmail.com>
2026-03-05 14:03:41 +0300

    + promises system

---
+++ MonoGameLibrary/Promises/IPromise.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MonoGameLibrary.Promises;
+
+public interface IPromise
+{
+    IPromise Done(Action callback);
+    IPromise Fail(Action<Exception> callback);
+    IPromise Always(Action callback);
+    IPromise Then(Func<IPromise> next);
+    IPromise<TNext> Then<TNext>(Func<IPromise<TNext>> next);
+}
+
+public interface IPromise<T> : IPromise
+{
+    IPromise<T> Done(Action<T> callback);
+    IPromise<TNext> Then<TNext>(Func<T, IPromise<TNext>> next);
+}
